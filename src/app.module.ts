2ed99d66142ea13232763aa7ed2fa118dfc0e127
essmoncif <moncefessaoudi@gmail.com>
2020-11-06 14:50:15 +0100

    import typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConnectionService } from './connection.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: ConnectionService
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
