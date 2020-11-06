5d215013f38b5841090f26a3de2585451f9bcfe7
essmoncif <moncefessaoudi@gmail.com>
2020-11-06 15:37:28 +0100

    providing filer & interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConnectionService } from './connection.service';
+import { HttpErrorFilter } from './shared/http-error.filter';
+import { LoggingInterceptor } from './shared/logging.interceptor';
 
 @Module({
   imports: [
@@ -10,7 +13,18 @@ import { ConnectionService } from './connection.service';
       useClass: ConnectionService
     })
   ],
+
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+    {
+        provide: APP_INTERCEPTOR,
+        useClass: LoggingInterceptor,
+    },
+    AppService
+  ],
 })
 export class AppModule {}
