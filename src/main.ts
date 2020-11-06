b4b0b8af1c17e15202b7a4136a539198883ea6d8
essmoncif <moncefessaoudi@gmail.com>
2020-11-06 22:02:05 +0100

    add logger fnc

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.APP_PORT || 4000);
+  Logger.log(`Server running on http://localhost:${process.env.APP_PORT}`, 'Bootstrap');
 }
 bootstrap();
