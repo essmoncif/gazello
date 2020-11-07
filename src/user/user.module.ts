21cf628fed44630cc36f4e1d6dd5f568b119d2ea
essmoncif <moncefessaoudi@gmail.com>
2020-11-07 13:33:28 +0100

    export user service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,14 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserEntity } from './user.entity';
+import { PostEntity } from 'src/post/post.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity, PostEntity])
   ],
   providers: [UserService],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService]
 })
 export class UserModule {}
