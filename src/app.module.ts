2498958d3ed2a861dd6128a435912c86d5df956b
essmoncif <moncefessaoudi@gmail.com>
2020-11-07 15:57:01 +0100

    final app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,18 @@ import { AppService } from './app.service';
 import { ConnectionService } from './connection.service';
 import { HttpErrorFilter } from './shared/http-error.filter';
 import { LoggingInterceptor } from './shared/logging.interceptor';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useClass: ConnectionService
-    })
+    }),
+    UserModule,
+    PostModule,
+    CommentModule
   ],
 
   controllers: [AppController],
